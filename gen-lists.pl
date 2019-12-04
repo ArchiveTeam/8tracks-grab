c6c5b999326479803c678a449566d15f1f74a5a0
mark rosetta <marked@gmail.com>
2019-12-03 23:16:27 -0600

    Fix mixed whitespace

--- gen-lists.pl
+++ gen-lists.pl
@@ -6,14 +6,13 @@ use autodie;
 my $lc = 0;
 my $fh;
 while(my $line=<>) {
-	if ($lc % 1000*100 == 0) {
+  if ($lc % 1000*100 == 0) {
     mkdir "/6BT/AT/ytll-items/" . sprintf("%03d",$lc/100/1000);
     chdir "/6BT/AT/ytll-items/" . sprintf("%03d",$lc/100/1000);
   }
-	if ($lc % 100 == 0) {
-		#close $fh;
-		open $fh, ">", sprintf("%d", ($lc/100) % 1000);
-	}
-	print $fh $line;
-	$lc++
+  if ($lc % 100 == 0) {
+    open $fh, ">", sprintf("%d", ($lc/100) % 1000);
+  }
+  print $fh $line;
+  $lc++
 }
